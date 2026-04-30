8cd1af9619bdc40c1c8d618c529c18a38220564d
Stephen Hull <9257454+StephenHull@users.noreply.github.com>
2026-04-30 19:02:01 -0400

    Add the Web Client and Web Server projects.

---
+++ WebServer/Data/ApplicationUser.cs
@@ -0,0 +1,8 @@
+using Microsoft.AspNetCore.Identity;
+
+namespace FoodAndNutrientData.Web.Server.Data;
+
+// Add profile data for application users by adding properties to the ApplicationUser class
+public class ApplicationUser : IdentityUser
+{
+}
